a2583e468af9c4ea9cfcdd0f81bca201359b72fa
Uzo AtHome <segmond@gmail.com>
2014-09-26 23:54:34 -0400

    adding new files

---
+++ project_euler/prob1.pl
@@ -0,0 +1,17 @@
+multipleOfM(M,N):-
+    0 is mod(N, M). 
+
+divisibleBy3or5(N):-
+    (multipleOfM(3,N) ; multipleOfM(5,N)).
+
+
+range(F,F,[F]).
+range(From, To, [From|L]):-
+    From < To,
+    F1 is From + 1,
+    range(F1, To, L).
+
+start(R2):-
+    range(1,1000,L),
+    include(divisibleBy3or5, L, R),
+    sum_list(R,R2).
---
+++ project_euler/prob2.pl
@@ -0,0 +1,10 @@
+fib(0, A, _, A).
+fib(N, A, B, F):-
+    N1 is N - 1,
+    Sum is A + B,
+    fib(N1, B, Sum,F).
+fib(N,F):-
+    fib(N, 0, 1, F).
+
+start:-
+    between(1, 100, N), fib(N,R), R > 4000000.
