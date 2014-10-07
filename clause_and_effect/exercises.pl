9d91ee01d6fb1bede356f7ea50f541a228c6d56e
Uzo AtHome <segmond@gmail.com>
2014-10-06 22:26:00 -0400

    dot product

--- clause_and_effect/exercises.pl
+++ clause_and_effect/exercises.pl
@@ -1,17 +1,17 @@
-member(E, [E|T]).
-member(E, [H|T]):-
+member(E, [E|_]).
+member(E, [_|T]):-
     member(E, T).
 
 mystery(X,A,B) :-
     member(X,A),
     member(X,B).
 
-coeff(A,X,B,Y) :-
+coeff(A,_,B,Y) :-
     Y is A * B + B.
 
 llength([], 0).
 
-llength([H|T], R) :-
+llength([_|T], R) :-
     llength(T, R1),
     R is R1 + 1.
 
@@ -21,7 +21,7 @@ tr_length(L, R) :-
     accumulate(L, 0, R).
 
 accumulate([], Acc, Acc).
-accumulate([H|T], Acc, R):-
+accumulate([_|T], Acc, R):-
     NewAcc is Acc + 1,
     accumulate(T, NewAcc, R).
 
@@ -30,3 +30,17 @@ sum([H|T], N) :-
     sum(T, N1),
     N is H+N1.
     
+% inner product, naive version
+inner([], [], 0).
+inner([H1|TVecA], [H2|TVecB], DotProduct):-
+    inner(TVecA, TVecB, NDotProduct),
+    DotProduct is NDotProduct + (H1 * H2).
+
+% inner accumlate
+tr_inner(V1, V2, DP):-
+    tr_inner(V1,V2,0,DP).
+
+tr_inner([],[],Acc,Acc).
+tr_inner([H1|TVecA], [H2|TVecB], Acc, DotProduct):-
+    NewAcc is Acc  + (H1 * H2),
+    tr_inner(TVecA, TVecB, NewAcc, DotProduct).
