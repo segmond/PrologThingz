a313c75632c780aa8b76b28bb67531db7cea5225
Uzo AtHome <segmond@gmail.com>
2014-10-09 19:27:13 -0400

    added no tail recursive collect

--- clause_and_effect/maps.pl
+++ clause_and_effect/maps.pl
@@ -159,6 +159,7 @@ collect_same([q(_,_)|T],A,DPart,R):-
 
 % as implemented in book
 
+% coll([q(17,duck), q(15,goose),q(41,quail),q(5,duck),q(12,goose),q(37,quail)], R)
 coll([],[]).
 coll([q(N,X)|R], [q(T,X)|R2]) :-
     collz(X,N,R,Q,T),
@@ -171,3 +172,15 @@ collz(X,N,[q(Num,X)|R],Q,T) :-
 collz(X,N,[Q|R],[Q|Qs],T) :-
     collz(X,N,R,Qs,T).
 
+% alternative that is not tail-recursive
+
+collect([], []).
+collect([q(N,X)|R], [q(T,X)|R3]):-
+    collect(R,R2),
+    extract(q(M,X),R2,R3),
+    T is M + N.
+extract(q(0,_),[],[]).
+extract(H,[H|T],T).
+extract(X,[Y|T],[Y|T1]):-
+    X \== Y,
+    extract(X,T,T1).
