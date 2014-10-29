eac84c0c945ed88219acf030ac21963290b2f703
Uzo AtHome <segmond@gmail.com>
2014-10-28 23:15:19 -0400

    term rewriting simpliciation

--- clause_and_effect/term_rewriting.pl
+++ clause_and_effect/term_rewriting.pl
@@ -13,3 +13,39 @@ d(A-B,X,U-V):-
 d(A*B,X,B*U+A*V):-
     d(A,X,U),
     d(B,X,V).
+
+% simplification
+% s(4+6+17-3*9+1*5, X).
+s(A+B,C):- !, s(A,A1),s(B,B1), oper(A1+B1,C).
+s(A-B,C):- !, s(A,A1),s(B,B1), oper(A1-B1,C).
+s(A*B,C):- !, s(A,A1),s(B,B1), oper(A1*B1,C).
+s(X,X).
+
+oper(A+B,C):-integer(A),integer(B),!,C is A + B.
+oper(A-B,C):-integer(A),integer(B),!,C is A - B.
+oper(A*B,C):-integer(A),integer(B),!,C is A * B.
+oper(0+A,A):- !.
+oper(A+0,A):- !.
+oper(1*A,A):- !.
+oper(0*A,0):- !.
+oper(A*1,A):- !.
+oper(A*0,0):- !.
+oper(A-0,A):- !.
+oper(A-A,0):- !.
+oper(X,X).
+
+
+% distribute negation
+% use DeMorgan's Laws to push negations inwards
+dn(-(-(A)),B):- !, dn(A,B).
+dn(-(A+B),U+V):- !, dn(-(A),U), dn(-(B),V).
+dn(-(A*B),U*V):- !, dn(-(A),U), dn(B,V).
+dn(A+B,U+V):- !, dn(A,U), dn(B,V).
+dn(A*B,U*V):- !, dn(A,U), dn(B,V).
+dn(A,A).
+
+/* simplify an expression */
+% simp(4+6+17-3*9+1*5, X).
+simp(X,Y):-
+    dn(X,A), s(A,Y).
+
--- dynamic_programming/memo.pl
+++ dynamic_programming/memo.pl
@@ -2,6 +2,9 @@
 memo(Goal) :- ( stored(Goal) -> true ; Goal, assertz(stored(Goal)) ).
 
 % tail recursive
+%?- time(fib(43,N)).
+% 88 inferences, 0.000 CPU in 0.000 seconds (97% CPU, 7961639 Lips)
+%N = 433494437 .
 fib(0, A, _, A).
 fib(N, A, B, F) :- 
     N1 is N - 1, 
@@ -12,6 +15,9 @@ fib(N, F) :-
     fib(N, 0, 1, F).
 
 % Naive
+%?- time(fibN(43,N)).
+% 2,104,226,197 inferences, 190.174 CPU in 190.911 seconds (100% CPU, 11064741 Lips)
+%N = 433494437.
 fibN(0, 0):- !.
 fibN(1, 1):- !.
 fibN(N, NF) :-
@@ -21,6 +27,9 @@ fibN(N, NF) :-
 
 % Memoized
 % faster than naive, but slower than tail recursive as values get larger
+%?- time(fibM(43,N)).
+% 382 inferences, 0.000 CPU in 0.000 seconds (99% CPU, 6564251 Lips)
+%N = 433494437
 fibM(0, 0).
 fibM(1, 1).
 fibM(N, NF):-
@@ -29,3 +38,19 @@ fibM(N, NF) :-
     A is N - 1, B is N - 2,
     memo(fibM(A, AF)), memo(fibM(B, BF)),
     NF is AF + BF.
+
+
+/* 
+ * Lisp implemenation in SBCL
+ * http://compgroups.net/comp.lang.lisp/prolog-in-lisp-2/1957845
+* (time (fib 42))
+
+Evaluation took:
+  2.599 seconds of real time
+  2.586676 seconds of total run time (2.586676 user, 0.000000 system)
+  99.54% CPU
+  7,778,409,814 processor cycles
+  0 bytes consed
+  
+433494437
+*/
