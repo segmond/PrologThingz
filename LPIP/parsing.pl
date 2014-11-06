5d19feff48983f21955a72660e61e261087bf7b3
Uzo AtHome <segmond@gmail.com>
2014-11-05 23:28:53 -0500

    more fun with DCG

--- LPIP/parsing.pl
+++ LPIP/parsing.pl
@@ -1,32 +1,48 @@
 % DCG
-sentence --> noun_phrase, compound_verb, noun_phrase.
-sentence --> noun_phrase, compound_verb.
+sentence --> noun_phrase(_), compound_verb(both), noun_phrase(_).
+sentence --> noun_phrase(_), compound_verb(both).
 
-noun_phrase --> determiner, adjective, noun.
-noun_phrase --> determiner, noun.
-noun_phrase --> noun.
+sentence --> noun_phrase(Plurality), compound_verb(Plurality), noun_phrase(_).
+sentence --> noun_phrase(Plurality), compound_verb(Plurality).
 
-compound_verb -->  auxiliary_verb, verb.
-compound_verb -->  verb.
+noun_phrase(Plurality) --> determiner, adjective_sequence, noun(Plurality).
+noun_phrase(Plurality) --> determiner, noun(Plurality).
+noun_phrase(Plurality) --> noun(Plurality).
+
+compound_verb(Plurality) -->  auxiliary_verb, verb(Plurality).
+compound_verb(Plurality) -->  verb(Plurality).
 
 auxiliary_verb --> [will].
 
-verb --> [eat].
-verb --> [saw].
-verb --> [see].
-verb --> [hears].
-verb --> [took].
-verb --> [sees].
+adjective_sequence --> adjective, adjective_sequence.
+adjective_sequence --> adjective.
+
+verb(plural) --> [eat].     % disguised form of the prolog clause verb([eat|A], A).
+verb(plural) --> [see].
+
+verb(both) --> [saw].
+verb(both) --> [took].
+verb(both) --> [sat].
+verb(both) --> [will_see].
+
+verb(singular) --> [hears].
+verb(singular) --> [sees].
 
 determiner --> [the].
 determiner --> [a].
 determiner --> [an].
 
-noun --> [cat].
-noun --> [mat].
-noun --> [man].
-noun --> [boy].
-noun --> [dog].
+noun(singular) --> [cat].
+noun(singular) --> [mat].
+noun(singular) --> [man].
+noun(singular) --> [boy].
+noun(singular) --> [dog].
+
+noun(plural) --> [cats].
+noun(plural) --> [mats].
+noun(plural) --> [men].
+noun(plural) --> [boys].
+noun(plural) --> [dogs].
 
 adjective --> [large].
 adjective --> [small].
@@ -55,3 +71,17 @@ test4:-
 
 test5:-
     phrase(sentence, [the, blue, cat, saw, the, large, man]).
+
+test6:-
+    phrase(sentence, [the, large, man, saw, the, small, brown, orange, dog]).
+
+
+test7:-
+    phrase(sentence, [the, small, green, man, sees, a, large, dog]).
+
+test8:- % will fail
+    phrase(sentence, [the, small, green, men, sees, a, large, dog]).
+
+test9:-
+    phrase(sentence, [the, small, green, men, saw, a, large, dog]).
+
