34a368cf91ed0f4414ca9f301d657d22ff84f786
Uzo AtHome <segmond@gmail.com>
2014-11-11 23:48:51 -0500

    stack code generation

--- clause_and_effect/compilation/cg_stack.pl
+++ clause_and_effect/compilation/cg_stack.pl
@@ -4,3 +4,30 @@ cg(I, pushc(I)):-
 
 cg(A, push(A)):-
     atom(A).
+
+cg(X+Y, [CX, CY, add]):-
+    cg(X, CX),
+    cg(Y, CY).
+
+cg(X*Y, [CX, CY, mul]):-
+    cg(X, CX),
+    cg(Y, CY).
+
+cg(X<Y, [CX, CY, clt]):-
+    cg(X, CX),
+    cg(Y, CY).
+
+cg(assign(X,Y), [CY, pop(X)]):-
+    cg(Y, CY).
+
+cg(while(X, S), [label(R1), CX, bz(R2), SX, br(R1), label(R2)]):-
+    cg(X, CX),
+    cg(S, SX).
+
+cg((A;B), [CA, CB]):-
+    cg(A, CA),
+    cg(B, CB).
+
+cg(if(X, Y), [CX, bz(R2), CY, label(R2)]):-
+    cg(X, CX),
+    cg(Y, CY).
