58b1a5e1e14afb70d96252508d9e7f0700062811
Uzo AtHome <segmond@gmail.com>
2014-11-15 22:18:29 -0500

    better command processing

--- LPIP/robot_control.pl
+++ LPIP/robot_control.pl
@@ -1,23 +1,47 @@
 % read in sequence of lines one at a time and write them out again until
 % a line containg just STOP is input as follows.
 
-go:-
+control_robot:-
 	repeat,
-	inputline([], L),
-	name(S, L),
-	write('S='),
-	writeln(S),
-	S =  'STOP',
-	writeln('FINISHED').
+	inputline(L),
+	L =  [stop],
+	writeout(L),
+	!.
 
-inputline(OldL, L):-
-	get0(X),
-	process(X, OldL, L).
+inputline(L):-
+	buildlist(L, []),
+	reverse(L, L1),
+	writeout(L1),
+	!.
 
-process(10, L, L).
-process(X, OldL, NewL):-
-	X =\= 10,
-	append(OldL, [X], L2),
-	inputline(L2, NewL).
+writeout([]).
+writeout(L):-
+	writeq(L),
+	nl.
 
+buildlist(L, OldL):-
+	findword(Word, []),
+	(
+	    (Word=[], L=OldL);
+	    (Word=[sep], buildlist(L, OldL));
+	    (Word=[terminator|Word1], name(S, Word1), L=[S|OldL]);
+	    (name(S,Word), buildlist(L, [S|OldL]))
+	).
+
+findword(Word, OldWord):-
+	get0(X1),
+	repchar(X1, X),
+	(
+	    (terminator(X), Word=[terminator|OldWord]);
+	    (separator(X), ((OldWord=[], Word=[sep]); Word = OldWord));
+	    (append(OldWord, [X], New), findword(Word, New))
+	).
+
+separator(32).    % separator
+
+terminator(10).   % end of line
+
+repchar(X, New):-
+	X >= 65, X =< 90, New is X + 32, !.
+repchar(Char, Char).
 
