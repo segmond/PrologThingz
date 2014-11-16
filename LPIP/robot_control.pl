3dbd8af05e633ea1d0a54a1d8d2f6ef088d57f66
Uzo AtHome <segmond@gmail.com>
2014-11-15 23:27:31 -0500

    orientation and remembering position

--- LPIP/robot_control.pl
+++ LPIP/robot_control.pl
@@ -1,11 +1,11 @@
 % read in sequence of lines one at a time and write them out again until
-% a line containg just STOP is input as follows.
+% a line containing just STOP is input as follows.
 
 control_robot:-
+	initialise,
 	repeat,
 	inputline(L),
 	L =  [stop],
-	writeout(L),
 	!.
 
 inputline(L):-
@@ -16,8 +16,14 @@ inputline(L):-
 
 writeout([]).
 writeout(L):-
-	writeq(L),
-	nl.
+	(   (verify(L),
+	     X =.. L,
+	     writeln(X),
+	     call(X)
+	    )
+	    ;
+	    (writeln("Invalid input"))
+	).
 
 buildlist(L, OldL):-
 	findword(Word, []),
@@ -44,4 +50,57 @@ terminator(10).   % end of line
 repchar(X, New):-
 	X >= 65, X =< 90, New is X + 32, !.
 repchar(Char, Char).
+
+
+initialise:-
+	retractall(orientation(_)),
+	retractall(position(_,_)),
+	assertz(position(0,0)),   % zero meters North, and zero meters East
+	assertz(orientation(90)). % degrees anticlose form East
+
+
+% verify that the command is valid
+verify([H|_]):-
+	member(H, [forward, back, turn, goto, face, report, stop]).
+
+% robot commands
+stop:-
+	writeln("End of Input").
+
+turn(right):-
+	turn(90, degrees, clockwise).
+turn(left):-
+	turn(90, degrees, anticlockwise).
+turn(round):-
+	turn(180, degrees, anticlockwise).
+turn(N, degrees, clockwise):-
+	N1 is -1 * N,
+	turn(N1, degrees, anticlockwise).
+turn(N, degrees, anticlockwise):-
+	retract(orientation(Current)),
+	New is (Current+N) mod 360,
+	assertz(orientation(New)),
+	format("** New orientation is ~w degreees anticlockwise from East~n", [New]).
+
+face(N, degrees):-
+	retract(orientation(_)),
+	assertz(orientation(N)),
+	format("** New orientation is ~w degreees anticlockwise from East~n", [N]).
+
+report:-
+	position(North, East),
+	round2dp(North, North1),
+	round2dp(East, East1),
+	format("** Position is ~w meters North and ~w meters East~n", [North1, East1]),
+	orientation(N),
+	format("** Orientation is ~w degreees anticlockwise from East~n", [N]).
+
+round2dp(X, Y):-
+	Y is round(X*100) / 100.
+
+
+
+
+
+
 
