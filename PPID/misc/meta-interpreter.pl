ab5c2b0c3ac6333d3b81a1724ca793c3d476f92c
Uzo AtHome <segmond@gmail.com>
2014-11-19 23:29:26 -0500

    meta

--- PPID/misc/meta-interpreter.pl
+++ PPID/misc/meta-interpreter.pl
@@ -13,4 +13,12 @@ interpret(Goal):-
 % test KB
 :-dynamic(dog/1).
 dog(fido).
+
 cat(felix).
+
+count_list([], 0).
+count_list([H|T], inc(N)):-
+    count_list(T, N).
+
+%    ?- interpret(count_list([a,b,c,d], X)).
+%    X = inc(inc(inc(inc(0)))).
