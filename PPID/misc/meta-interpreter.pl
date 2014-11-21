fcf8551c4ab2a18fcc7bcc870c2b4a7a99e9a195
Uzo AtHome <segmond@gmail.com>
2014-11-20 23:07:49 -0500

    more meta

--- PPID/misc/meta-interpreter.pl
+++ PPID/misc/meta-interpreter.pl
@@ -16,9 +16,25 @@ dog(fido).
 
 cat(felix).
 
+parent(michael, cathy).
+parent(melody, cathy).
+parent(charles_gordon, michael).
+parent(hazel, michael).
+parent(jim, melody).
+parent(eleanor, melody).
+
+grandparent(X, Y):-
+    parent(Z, Y),
+    parent(X, Z).
+
 count_list([], 0).
 count_list([H|T], inc(N)):-
     count_list(T, N).
 
 %    ?- interpret(count_list([a,b,c,d], X)).
 %    X = inc(inc(inc(inc(0)))).
+
+test:-
+    interpret(grandparent(A,B)),
+    writeln([A,B]),
+    fail.
