0c7fd7539ee087c98574355956afcac01bb55381
Uzo AtHome <segmond@gmail.com>
2014-11-23 17:52:04 -0500

    more complete rewrite term

--- PPID/misc/rewrite_term.pl
+++ PPID/misc/rewrite_term.pl
@@ -1,13 +1,42 @@
 %
-rewrite_term(T, R):-
+my_rewrite_term(T, R):-
     T =.. A,
     rewrite_term_aux(A, R).
 
+rewrite_term_aux([], []).
 rewrite_term_aux([b|T], [a|R]):-
     rewrite_term_aux(T, R).
-
 rewrite_term_aux([H|T], [H|R]):-
     rewrite_term_aux(T, R).
 
-rewrite_term_aux([], []).
     
+
+% section 7.1
+rewrite(a, b):-  % change all a to b
+    !.
+rewrite(X, X).  % leave everything alone
+
+rewrite_term(X, X):-
+    var(X),
+    !.
+
+rewrite_term(X, Y):-
+    atomic(X),
+    !,
+    rewrite(X, Y).
+
+rewrite_term(X, Y):-
+    X =.. XList,
+    rewrite_aux(XList, YList),
+    Y =.. YList.
+
+rewrite_aux([], []).
+rewrite_aux([First|Rest], [NewFirst|NewRest]):-
+    rewrite_term(First, NewFirst),
+    rewrite_aux(Rest, NewRest).
+
+/*
+?- my_rewrite_term(foo(a,[b,a(X), d]), X).
+X = [foo, a, [b, a(X), d]].
+*/
+
