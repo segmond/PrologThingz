18741b8ea61f101aed36d4c0fa25ba1860ae7e69
Uzo AtHome <segmond@gmail.com>
2014-11-25 22:07:18 -0500

    quicksort

--- PPID/sorts/qsort.pl
+++ PPID/sorts/qsort.pl
@@ -1,10 +1,14 @@
-partition([X|Tail], Pivot, [X|Before], After):-
-    X @< Pivot, 
-    !,
-    partition(Tail, Pivot, Before, After).
-
-partition([X|Tail], Pivot, Before, [X|After]):-
-    !,
-    partition(Tail, Pivot, Before, After).
-
 partition([], _, [], []).
+partition([X|Tail], Pivot, [X|Before], After):-
+    X @< Pivot, !,
+    partition(Tail, Pivot, Before, After).
+partition([X|Tail], Pivot, Before, [X|After]):-
+    !, partition(Tail, Pivot, Before, After).
+
+
+quicksort([], []).
+quicksort([X|Tail], Result):-
+    !, partition(Tail, X, Before, After),
+    quicksort(Before, SortedBefore),
+    quicksort(After, SortedAfter),
+    append(SortedBefore, [X|SortedAfter], Result).
