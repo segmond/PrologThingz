ce8e129a4eb7189da3e6fae42d8d6c61b8024322
Uzo AtHome <segmond@gmail.com>
2014-12-01 01:05:58 -0500

    better simplification although too verbose

--- PPAI/simplify.pl
+++ PPAI/simplify.pl
@@ -1,21 +1,63 @@
 % ex 6.1
-% terrible, need to fix
-s(X+Y, C):- !,
-    s(X, XS),
-    s(Y, YS),
-    oper(XS+YS,C).
-s(X, X).
-oper(X+Y, X+Y):-
-    number(X), atom(Y), !.
-oper(X+Y, Y+X):-
-    atom(X), number(Y), !.
-oper(X+Y, X+X):-
-    atom(X), atom(Y), !.
-oper(X+X, 2*X):- atom(X), !.
-oper(X+Y, E):-
+% a bit better
+
+% simplify an expression
+simplify(Expr, Simplified):-
+    simplify_aux(Expr, DirtyExpr),
+    clean_up(DirtyExpr, Simplified).
+
+% simplify, shift atoms to the left and numbers to the right
+simplify_aux(X+Y, C):-
     number(X), number(Y), !,
-    E is X+Y.
-oper(X,X).
+    C is X+Y.
+simplify_aux(X+Y, Y+X):-
+    number(X), atom(Y), !. 
+simplify_aux(X+Y, X+Y):- 
+    atom(X), number(Y), !. 
+simplify_aux(X+Y, XS+Y):-
+    compound(X), number(Y), !,
+    simplify_aux(X, XS).
+simplify_aux(X+Y, Y+XS):-
+    compound(X), atom(Y), !,
+    simplify_aux(X, XS).
+simplify_aux(X, X).
+
+
+% clean up till there are no paranthesis
+clean_up(A, A):-
+    is_clean(A).
+
+clean_up(A, D):-
+    \+ is_clean(A),
+    clean(A, C),
+    clean_up(C, D).
+    
+% remove one layer of paranethesis
+clean(X+Y, A+B+YS):-
+    compound(X),
+    clean(X, A+B),
+    clean(Y, YS).
+clean(X+Y, XS+A+B):-
+    compound(Y),
+    clean(X, XS),
+    clean(Y, A+B).
+clean(X+Y, X+Y):-
+    atomic(X), atomic(Y).
+clean(X,X):-
+    atomic(X).
+
+% check if all paranthesis are gone
+is_clean(X+Y):-
+    atomic(Y),
+    is_clean(X).
+is_clean(X):-
+    atomic(X).
+
+test:-
+    simplify(c+(b+(a+1+4+2)), X),
+    writeln(X).
+
+
 
 
 % ex 6.2
