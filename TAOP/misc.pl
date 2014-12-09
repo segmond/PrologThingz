4d4efe67676862ae1582a6dc19dc79d7695915ef
Uzo AtHome <segmond@gmail.com>
2014-12-08 23:27:19 -0500

    misc

---
+++ TAOP/misc.pl
@@ -0,0 +1,32 @@
+dosquares:-
+    repeat,
+    read(X),
+    (
+        X = stop, !
+        ;
+        Y is X*X, writeln(Y),
+        fail
+    ).
+
+age(peter, 7).
+age(ann, 5).
+age(pat, 8).
+age(tom, 5).
+
+myfindall(X, Goal, Xlist):-
+    call(Goal),
+    assertz(queue(X)),
+    fail;
+    assertz(queue(bottom)),
+    collect(Xlist).
+
+collect(L):-
+    retract(queue(X)), !,           % retract next solution
+    ( X == bottom, !, L = []        % end of solutions?
+    ;
+    L = [X|Rest], collect(Rest)).   % Otherwise add X and collect the rest
+
+test:-
+    myfindall(Child, age(Child, Age), List1),
+    findall(Child, age(Child, Age), List2),
+    List1 = List2.
