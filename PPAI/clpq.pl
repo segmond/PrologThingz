c194f938bea30c5fb28247ccb2af402ecfd72b31
Uzo AtHome <segmond@gmail.com>
2014-12-15 02:15:14 -0500

    clp

--- PPAI/clpq.pl
+++ PPAI/clpq.pl
@@ -25,3 +25,15 @@ fib(N, A, B, F) :-
 clp_fib(N, F) :- 
     fib(N, 0, 1, F).
 
+
+clp_plus(X, Y, Z):-
+    {Z = X + Y}.
+
+fact(N, F):-
+    {N = 0, F = 1 };
+    {
+        N > 0,
+        N1 = N - 1,
+        F = N * F1
+    },
+    fact(N1, F1).
