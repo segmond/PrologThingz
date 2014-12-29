a55ff274fdad284818514aa97da4c0ee1ff79d8e
Uzo AtHome <segmond@gmail.com>
2014-12-28 20:50:04 -0500

    unify

--- TAOP/unify.pl
+++ TAOP/unify.pl
@@ -15,3 +15,13 @@ unify(X,Y):-
 
 unify(X,Y):-
     nonvar(X), nonvar(Y), compound(X), compound(Y), term_unify(X,Y).
+
+term_unify(X, Y):-
+    functor(X, F, N), functor(Y, F, N), unify_args(N, X, Y).
+
+unify_args(N, X, Y):-
+    N > 0, unify_arg(N, X, Y), N1 is N-1, unify_args(N1, X, Y).
+unify_args(0, X, Y).
+
+unify_arg(N, X, Y):-
+    arg(N, X, ArgX), arg(N, Y, ArgY), unify(ArgX, Argy).
