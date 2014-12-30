92048caee5f3168bbcb48d4960f7a708be8fa112
Uzo AtHome <segmond@gmail.com>
2014-12-29 23:31:46 -0500

    unify

--- TAOP/unify_with_occurs.pl
+++ TAOP/unify_with_occurs.pl
@@ -1,14 +1,15 @@
 % unify(Term1, Term2)
 %   Term1 and Term2 are unified, ignoring the occurs check.
 
+
 unify(X,Y):-
     var(X), var(Y), X=Y.
 
 unify(X,Y):-
-    var(X), nonvar(Y), X=Y.
+    var(X), nonvar(Y), not_occurs_in(X,Y), X=Y.
 
 unify(X,Y):-
-    var(X), nonvar(Y), Y=X.
+    var(X), nonvar(Y), not_occurs_in(Y,X), Y=X.
 
 unify(X,Y):-
     nonvar(X), nonvar(Y), atomic(X), atomic(Y), X=Y.
@@ -25,6 +26,25 @@ unify_args(0, X, Y).
 
 unify_arg(N, X, Y):-
     arg(N, X, ArgX), arg(N, Y, ArgY), unify(ArgX, Argy).
+
+not_occurs_in(X, Y):-
+    var(Y), X \== Y.
+
+not_occurs_in(X, Y):-
+    nonvar(Y), atomic(Y).
+
+not_occurs_in(X, Y):-
+    nonvar(Y), atomic(Y), functor(Y, F, N), not_occurs_in(N, X, Y).
+    
+
+not_occurs_in(X, Y):-
+    N > 0, 
+    arg(N, Y, Arg),
+    not_occurs_in(X, Arg),
+    N1 is N-1,
+    not_occurs_in(N1, X, Y).
+
+not_occurs_in(0, X, Y).
 
 test1:-
     unify(X, A).
